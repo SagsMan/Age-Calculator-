1b625ed4e7a279d70d6a8a659cbdc521cf9989a8
Sagiru Garba <122167371+SagsMan@users.noreply.github.com>
2024-07-19 18:01:55 +0100

    Add files via upload
    SagsMan design this Age calculator to ease age calculating of anything to anyone

---
+++ Age calculator python.py
@@ -0,0 +1,17 @@
+a = int(input("Enter Present Year: "))
+b = int(input("Enter your Birth-year: "))
+c = input("Month of Birth: ") 
+d = input("Date of Birth: ")
+e = input("Your State: ")
+
+name = input("Name: ")
+print("Dear", name)
+print("you where Born in the Month of", c)
+x = a-b
+
+print("your age is:", x) 
+print("and you'll be", x, "of age by", c, "this year")
+print("in Nutshell", name,  "is from", e, "State")
+
+
+
